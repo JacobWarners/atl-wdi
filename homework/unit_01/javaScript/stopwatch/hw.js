801555d8afb5eff9d74f6b39528ef6c154b4f867
whuizenga <will.huizenga@gmail.com>
2017-07-11 08:56:44 -0400

    Got everything complete except for the lap display

--- homework/unit_01/javaScript/stopwatch/hw.js
+++ homework/unit_01/javaScript/stopwatch/hw.js
@@ -18,29 +18,89 @@ const Stopwatch = {
   laps: [],
   // DO NOT EDIT ABOVE THIS LINE
   advanceTenMillisecs: function(){
-    // Your Code Here
+    Stopwatch.millisecs += 10;
+    if(Stopwatch.millisecs > 990){
+      Stopwatch.secs += 1;
+      Stopwatch.millisecs = 0;
+    };
+    if(Stopwatch.secs === 60){
+      Stopwatch.mins += 1;
+      Stopwatch.secs = 0;
+    }
+    ViewEngine.updateTimeDisplay(Stopwatch.mins, Stopwatch.secs, Stopwatch.millisecs);
   },
   reset: function(){
-    // Your Code Here
+    $(".lap").remove();
+    Stopwatch.millisecs = 0;
+    Stopwatch.secs = 0;
+    Stopwatch.mins = 0;
+    ViewEngine.updateTimeDisplay(Stopwatch.mins, Stopwatch.secs, Stopwatch.millisecs);
   },
   start: function(){
-    // Your Code Here
+    Stopwatch.isRunning = true;
+    Stopwatch.tickClock();
   },
   stop: function(){
-    // Your Code Here
+    if(Stopwatch.isRunning){
+    Stopwatch.isRunning = false;
+  } else {
+    Stopwatch.reset();
+  }
   },
   lap: function(){
-    // Your Code Here
+    var min = "";
+    var sec = "";
+    var milsec = "";
+    //console.log(Stopwatch.mins+":"+Stopwatch.secs+":"+Stopwatch.millisecs);
+    if(Stopwatch.mins > 9){
+      min = toString(Stopwatch.mins);
+    } else {
+      min = "0"+toString(Stopwatch.mins);
+    }
+    if(Stopwatch.secs > 9){
+      sec = toString(Stopwatch.secs);
+    } else {
+      sec = "0"+toString(Stopwatch.secs);
+    }
+    if(Stopwatch.millisecs > 90){
+      milsec = toString(Stopwatch.millisecs);
+    } else {
+      milsec = "0"+toString(Stopwatch.millisecs);
+    }
+    var newLap = min+":"+sec+":"+milsec;
+    console.log(newLap);
   }
 };
 
 /// User Interface ///
 const ViewEngine = {
   updateTimeDisplay: function(mins, secs, millisecs){
-    // Your Code Here
+    if(mins >9){
+    $("#mins").html(mins);
+    } else {
+      $("#mins").html("0"+mins);
+    }
+    if(secs >9){
+    $("#secs").html(secs);
+    } else {
+      $("#secs").html("0"+secs);
+    }
+    if(millisecs > 99){
+    $("#millisecs").html(millisecs);
+    } else {
+      $("#millisecs").html("0"+millisecs);
+    }
+
+    if(millisecs === 0){
+      $("#millisecs").html("000");
+    }
+      
   },
   updateLapListDisplay: function(laps){
-    // Your Code Here
+    $newLi = $("<li>");
+    $newLi.addClass("lap");
+    $newLi.html("<span id='mins'>00</span>:<span id='secs'>00</span>:<span id='millisecs'>000</span>");
+    $("lap-list").append($newLi);
   },
 };
 const ViewHelpers = {
@@ -52,10 +112,11 @@ const ViewHelpers = {
 /// Top-Level Application Code ///
 const AppController = {
   handleClockTick: function(){
-    // Your Code Here
+    Stopwatch.tickClock;
   },
-  handleClickStart: function() {
-    // Your Code Here
+  handleClickStart: function(event) {
+    $("#start").on("click", Stopwatch.tickClock())
+    console.log("click start");
   },
   handleClickStopReset: function(){
     // Your Code Here
@@ -66,5 +127,16 @@ const AppController = {
 };
 
 window.onload = function(){
-  // Attach AppController methods to the DOM as event handlers here.
-};
+  $("#start").on("click", function(event){
+    event.preventDefault();
+    Stopwatch.start();
+  });
+  $("#stop").on("click", function(event){
+    event.preventDefault();
+    Stopwatch.stop();
+  });
+  $('#lap').on("click", function(event){
+    event.preventDefault();
+    Stopwatch.lap();
+  });
+  };
