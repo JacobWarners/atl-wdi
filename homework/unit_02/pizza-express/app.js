58b9d1d3eca37aa816cceac8e2de10d587557252
whuizenga <will.huizenga@gmail.com>
2017-07-19 23:45:03 -0400

    completed GET routes

--- homework/unit_02/pizza-express/app.js
+++ homework/unit_02/pizza-express/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 var app = express();
 
+app.get("/", (req, res) => {
+    res.send("Welcome to Pizza Express!");
+});
 
+app.get("/topping/:type", (req, res) => {
+    res.send(`${req.params.type} pizza! Good choice!`);
+});
+
+app.get("/order/:amount/:size", (req, res) => {
+    res.send(`Your order of ${req.params.amount} ${req.params.size} pizza(s) will be ready in a moment!`);
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
