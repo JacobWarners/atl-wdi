7a62cd32fce99efc7a9ab2efd6944db0da1ec76c
whuizenga <will.huizenga@gmail.com>
2017-07-20 22:20:05 -0400

    new routes created

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -3,11 +3,15 @@ const router = express.Router();
 var pirates = require('../models/pirates.js');
 
 router.get("/", (req, res) => {
-    res.render("index")
+    res.render("pirates/index")
+});
+
+router.get("/new", (req, res) => {
+    res.render("pirates/new")
 });
 
 router.get("/:id", (req, res) => {
-
+    res.render("pirates/show")
 });
 
 
