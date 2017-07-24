12000d19c2d65b0bfa51777a2e5cb1f3710d73eb
whuizenga <will.huizenga@gmail.com>
2017-07-24 19:52:18 -0400

    part 1: created tamagachi consturctor

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -1,9 +1,23 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
-
+class Tamagotchi {
+    constructor(name){
+        this.name = name,
+        this.foodInTummy = 10,
+        this.restedness = 10,
+        this.health = 10
+    }
+    cry() {
+        this.foodInTummy -= 1;
+        console.log(`${this.name} cries.. WAHH!`);
+    }
+}
 
 //create new Tamagotchis
-
+const cecil = new Tamagotchi("Cecil");
+const kate = new Tamagotchi("Kate");
 
 //test out your Tamagotchies below via console.logs
+cecil.cry();
+kate.cry();
--- labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
+++ labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
@@ -4,19 +4,38 @@ const fs = require('fs');
 
 let inFile = process.argv[2];
 
-fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
-  if (error) {
-    console.error(error);
-  }
+// fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+//   if (error) {
+//     console.error(error);
+//   }
 
-  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
+//   // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
 
-  let lines = content.split('\n');
+//   let lines = content.split('\n');
 
-  // clean up the array by removing the empty line
-  lines.pop();
+//   // clean up the array by removing the empty line
+//   lines.pop();
 
-  lines.forEach(function(line) {
-    console.log('Hello, ' + line + '!');
-  });
+//   lines.forEach(function(line) {
+//     console.log('Hello, ' + line + '!');
+//   });
+// });
+
+let readFilePromise = new Promise((resolve, reject) => {
+  fs.readFile(inFile, { endcoding: 'utf8' }, function(error, content){
+      reject(error);
+      resolve(content);
+    });
 });
+
+readFilePromise
+        .then((content) => {
+          let lines = content.split('\n');
+          lines.pop();
+          lines.forEach(function(line) {
+              console.log('Hello, ' + line + '!');
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
