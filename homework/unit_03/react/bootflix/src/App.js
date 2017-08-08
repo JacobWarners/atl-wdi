3d4008505480a75b365fc5426ada629ec530d3e5
whuizenga <will.huizenga@gmail.com>
2017-08-07 23:05:11 -0400

    I finished. It works. Im so happy

--- homework/unit_03/react/bootflix/src/App.js
+++ homework/unit_03/react/bootflix/src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
-import example from './omdbExample.json'
 
 class App extends Component {
   constructor(){
     super();
+
     this.state = {
-      movie: example
+      title: "",
+      year: 0,
+      director: "",
+      plot: "",
+      genre: "",
+      poster: "",
     }
   }
 
   //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
   _searchByTitle = (event) => {
-    console.log("Search by Title");
+    event.preventDefault();
+    const title = event.target.title.value;
+    axios.get(`http://www.omdbapi.com/?t=${title}&apikey=d31f1a94`)
+      .then((res) => {
+        this.setState({
+          title: res.data.Title,
+          year: res.data.Year,
+          director: res.data.Director,
+          plot: res.data.Plot,
+          genre: res.data.Genre,
+          poster: res.data.imdbID,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     
   }
 
   _searchById = (event) => {
-    console.log("Search by ID");
+    event.preventDefault();
+    const id = event.target.id.value;
+    axios.get(`http://www.omdbapi.com/?i=${id}&apikey=d31f1a94`)
+      .then((res) => {
+        this.setState({
+          title: res.data.Title,
+          year: res.data.Year,
+          director: res.data.Director,
+          plot: res.data.Plot,
+          genre: res.data.Genre,
+          poster: res.data.imdbID
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   //Pass _searchByTitle, _searchById, and this.state.movie to it's appropriate child components.
@@ -29,10 +65,17 @@ class App extends Component {
       <div className="App">
         <Header />
         <Search 
-          searchByTitle = {this._searchByTitle()}
-          searchById = {this._searchById()}
+          searchByTitle = {this._searchByTitle}
+          searchById = {this._searchById}
           />
-        <Movie />
+        <Movie 
+            title={this.state.title || "Search for a movie"}
+            year={this.state.year || "search"}
+            director={this.state.director || "find a movie"}
+            plot={this.state.plot || "Search for a movie using the search bar above."}
+            genre={this.state.genre || "exploration"}
+            poster={this.state.poster}
+            />
       </div>
     );
   }
--- homework/unit_03/react/bootflix/src/components/Search.jsx
+++ homework/unit_03/react/bootflix/src/components/Search.jsx
@@ -1,34 +1,21 @@
 import React from 'react';
 
 // Update the forms to utilize methods being passed down from App.js
-const Search = (event) => {
-    const searchByTitle = (event) => {
-      event.preventDefault();
-      console.log("click title")
-    } 
-    // props.searchByTitle;
-    const searchById = (event) => {
-      event.preventDefault();
-      console.log("click id")
-    }
-    
-    // props.searchById;
-
-    
+const Search = (props) => {
 
   return (
     <section id="movie-search">
       <strong>Search by:</strong> Title <em>or</em> ID
 
       <div className="search">
-        <form id="title-search-form" method="get" onSubmit={searchByTitle}>
+        <form id="title-search-form" method="get" onSubmit={props.searchByTitle}>
           <input type="text" name="title" placeholder="Enter movie title" />
           <input type="submit" value="Search for this title"/>
         </form>
       </div>
 
       <div className="search">
-        <form id="id-search-form" method="get" onSubmit={searchById}>
+        <form id="id-search-form" method="get" onSubmit={props.searchById}>
           <input type="text" name="id" placeholder="Enter omdb movie ID" />
           <input type="submit" value="Search by ID" />
         </form>
