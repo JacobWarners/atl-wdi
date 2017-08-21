c2a32de768addaaeda814baf614e2b4d27b4810c
whuizenga <will.huizenga@gmail.com>
2017-08-21 19:18:11 -0400

    arrays skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -34,8 +34,17 @@ g = "2"
 gToNumber = 2
 puts g.to_i + gToNumber
 
-puts "What is your name?"
-name = gets.chomp
-puts "What do you like to do?"
-activity = gets.chomp
-puts "Your name is #{name} and you like to #{activity}"
+# puts "What is your name?"
+# name = gets.chomp
+# puts "What do you like to do?"
+# activity = gets.chomp
+# puts "Your name is #{name} and you like to #{activity}"
+
+myFirstArray = []
+myFirstArray = ["one", "two", "three", "four", "five", 6, 7, 8, 9]
+puts myFirstArray[myFirstArray.length-1]
+puts myFirstArray[0]
+myFirstArray[1] = "Joe"
+myFirstArray[2] = 3
+myFirstArray << "true"
+puts myFirstArray[3].class
